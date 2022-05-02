650f2ea51ce038453eb1a12332900cc8e934df0a
shaden <92798768+shadenGhasri@users.noreply.github.com>
2022-05-02 15:44:18 +0430

    commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <></>
-  );
+    <BrowserRouter>
+      <Routes>
+        
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
