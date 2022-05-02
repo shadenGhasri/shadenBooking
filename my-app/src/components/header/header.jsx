fa67af5652be31c3e1a9f0b6e8e24ae814755933
shaden <92798768+shadenGhasri@users.noreply.github.com>
2022-05-03 01:02:09 +0430

    commit

--- my-app/src/components/header/header.jsx
+++ my-app/src/components/header/header.jsx
@@ -10,29 +10,57 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Header = () => {
-    
   return (
     <div className="header">
-      <div className="headerList">
-        <div className="headerListItem">
-          <FontAwesomeIcon icon={faBed} />
-          <span>Stays</span>
+      <div className="headerContainer ">
+        <div className="headerList">
+          <div className="headerListItem active">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Stays</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faPlane} />
+            <span>Flights</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faCar} />
+            <span>Car rentals</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Attractions</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faTaxi} />
+            <span>Airport taxis</span>
+          </div>
         </div>
-        <div className="headerListItem">
-          <FontAwesomeIcon icon={faPlane} />
-          <span>Flights</span>
-        </div>
-        <div className="headerListItem">
-          <FontAwesomeIcon icon={faCar} />
-          <span>Car rentals</span>
-        </div>
-        <div className="headerListItem">
-          <FontAwesomeIcon icon={faBed} />
-          <span>Attractions</span>
-        </div>
-        <div className="headerListItem">
-          <FontAwesomeIcon icon={faTaxi} />
-          <span>Airport taxis</span>
+        <h1 className="headerTitle">A lifetime of discounts? It's Genius.</h1>
+        <p className="headerDesc">
+          Get rewarded for your travels – unlock instant savings of 10% or more
+          with a free Lamabooking account
+        </p>
+        <button className="headerBtn">Sign in / Register</button>
+        <div className="headerSearch">
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
+            <input
+              type="text"
+              placeholder="Where are you going?"
+              className="headerSearchInput"
+            />
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faBed} className="headerIcon" />
+            <span className="headerSearchText">datee</span>
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faPerson} className="headerIcon" />
+            <span className="headerSearchText">room </span>
+          </div>
+          <div className="headerSearchItem">
+            <button className="headerBtn">search</button>
+          </div>
         </div>
       </div>
     </div>
