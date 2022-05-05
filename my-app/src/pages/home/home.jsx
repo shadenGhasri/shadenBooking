6efbaa9040f3ae20696c614bcf94bc9c6fd8daf9
shaden <92798768+shadenGhasri@users.noreply.github.com>
2022-05-05 16:08:37 +0430

    commit

--- my-app/src/pages/home/home.jsx
+++ my-app/src/pages/home/home.jsx
@@ -4,20 +4,23 @@ import Header from "../../components/header/header";
 import Featured from "../../components/featured/featured";
 import PropertyList from "../../components/propertyList/propertyList";
 import FeaturedProperties from "../../components/featuredProperties/featuredProperties";
-
+import MailList from "../../components/mailList/mailList";
 
 const Home = () => {
-    return (<>
-        <Navbar/>
-        <Header/>
-        <div className="homeContainer">
-            <Featured/>
-            <h1 className="homeTitle">Browse by property type</h1>
-            <PropertyList/>
-            <h1 className="homeTitle">Homes guests love</h1>
-            <FeaturedProperties/>
-        </div>
-    </>);
-}
- 
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList />
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties />
+        <MailList />
+      </div>
+    </>
+  );
+};
+
 export default Home;
