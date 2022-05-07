ff5c83a47ae60964b9eef7d27e06507f74a46cd3
shaden <92798768+shadenGhasri@users.noreply.github.com>
2022-05-07 14:09:50 +0430

    commit

--- my-app/src/pages/hotel/hotel.jsx
+++ my-app/src/pages/hotel/hotel.jsx
@@ -1,7 +1,20 @@
-import "./hotel.scss"
+import "./hotel.scss";
+import Navbar from "../../components/navbar/navbar";
+import Header from "../../components/header/header";
+import MailList from "../../components/mailList/mailList";
+import Footer from "../../components/footer/footer";
 
 const Hotel = () => {
-    return ( <p>hi</p> );
-}
- 
+  return (
+    <>
+      <Navbar />
+      <Header type="list" />
+
+      <MailList />
+      <Footer />
+      
+    </>
+  );
+};
+
 export default Hotel;
