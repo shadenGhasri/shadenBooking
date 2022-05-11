8702d10db558edec9b328c2dc5c99680511022b7
shaden <92798768+shadenGhasri@users.noreply.github.com>
2022-05-11 11:25:57 +0430

    commit

--- my-app/src/pages/login/login.jsx
+++ my-app/src/pages/login/login.jsx
@@ -1,19 +1,35 @@
+import { useState } from "react";
 import "./login.scss";
 
 const Login = () => {
+    const [account,setAccount] = useState({
+        username : "" ,
+        password : ""
+    })
+   
+    
     const handleSubmit = (e)=>{
         e.preventDefault();
+    }
+
+    const handleInput = (e)=>{
+        setAccount(p =>{
+            return {
+                ...p ,
+                [e.target.name] : e.target.value
+            }
+        })
     }
     return ( 
         <form onSubmit={handleSubmit} className="form d-flex flex-column align-items-center">
             <h1 className="mt-4">create an account</h1>
-            <input className="username mt-4" type="text" placeholder="Username"/>
-            <input className="password" type="password" name="" id="" placeholder="Password" />
+            <input className="username mt-4" type="text" placeholder="Username" name="username" value={account.username} onChange={handleInput} />
+            <input className="password" type="password" placeholder="Password" value={account.password} name="password" onChange={handleInput} />
             <button className="login">Login</button>
             <p className="mt-2">forget Login?</p>
-            {/* <div className="line">
+            <div className="line">
                 <h3 className="section-title">or</h3>
-            </div> */}
+            </div>
 
             <div className="media d-flex justify-content-center">
                 <div className="linkedin"><i className="bi bi-linkedin"></i></div>
