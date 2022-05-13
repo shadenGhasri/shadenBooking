ca046aac4afb1ff68a4fc0de8b3fb0f46bf6a643
shaden <92798768+shadenGhasri@users.noreply.github.com>
2022-05-14 01:29:22 +0430

    commit

--- my-app/src/components/featured/featured.jsx
+++ my-app/src/components/featured/featured.jsx
@@ -4,7 +4,7 @@ const Featured = () => {
   return (
     <div className="featured">
       <div className="row">
-        <div className="featuredItem col-md-4">
+        <div className="featuredItem col-sm-4">
           <img
             src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
             alt=""
@@ -12,11 +12,11 @@ const Featured = () => {
           />
           <div className="featuredTitles">
             <h1 className ="featuredTitle">Dublin</h1>
-            <h2 className ="featuredTitle">123 properties</h2>
+            <h4 className ="featuredTitle">123 properties</h4>
           </div>
         </div>
 
-        <div className="featuredItem col-md-4">
+        <div className="featuredItem col-sm-4">
           <img
             src="https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o="
             alt=""
@@ -24,10 +24,10 @@ const Featured = () => {
           />
           <div className="featuredTitles">
             <h1 className ="featuredTitle">Reno</h1>
-            <h2 className ="featuredTitle">533 properties</h2>
+            <h4 className ="featuredTitle">533 properties</h4>
           </div>
         </div>
-        <div className="featuredItem col-md-4">
+        <div className="featuredItem col-sm-4">
           <img
             src="https://cf.bstatic.com/xdata/images/city/max500/689422.webp?k=2595c93e7e067b9ba95f90713f80ba6e5fa88a66e6e55600bd27a5128808fdf2&o="
             alt=""
@@ -35,7 +35,7 @@ const Featured = () => {
           />
           <div className="featuredTitles">
             <h1 className ="featuredTitle">Austin</h1>
-            <h2 className ="featuredTitle">532 properties</h2>
+            <h4 className ="featuredTitle">532 properties</h4>
           </div>
         </div>
       </div>
--- my-app/src/components/propertyList/propertyList.jsx
+++ my-app/src/components/propertyList/propertyList.jsx
@@ -4,7 +4,7 @@ const PropertyList = () => {
   return (
     <div className="pList">
       <div className="row">
-      <div className="pListItem col-md-2">
+      <div className="pListItem col-lg-4 col-md-4 col-sm-4 col-xs-2">
         <img
           src="https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o="
           alt=""
@@ -15,7 +15,7 @@ const PropertyList = () => {
           <h2>859,199 hotels</h2>
         </div>
       </div>
-      <div className="pListItem col-md-2">
+      <div className="pListItem col-lg-4 col-md-4 col-sm-4 col-xs-2">
         <img
           src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg"
           alt=""
@@ -26,7 +26,7 @@ const PropertyList = () => {
           <h2>809,631 apartments</h2>
         </div>
       </div>
-      <div className="pListItem col-md-2">
+      <div className="pListItem col-lg-4 col-md-4 col-sm-4 col-xs-2">
         <img
           src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/bg_resorts/6f87c6143fbd51a0bb5d15ca3b9cf84211ab0884.jpg"
           alt=""
@@ -37,7 +37,7 @@ const PropertyList = () => {
           <h2>17,769 resorts</h2>
         </div>
       </div>
-      <div className="pListItem col-md-2">
+      <div className="pListItem col-lg-4 col-md-4 col-sm-4 col-xs-2">
         <img
           src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-villas_300/dd0d7f8202676306a661aa4f0cf1ffab31286211.jpg"
           alt=""
@@ -48,7 +48,7 @@ const PropertyList = () => {
           <h2>422,318 villas</h2>
         </div>
       </div>
-      <div className="pListItem col-md-2">
+      <div className="pListItem col-lg-4 col-md-4 col-sm-4 col-xs-2">
         <img
           src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-chalet_300/8ee014fcc493cb3334e25893a1dee8c6d36ed0ba.jpg"
           alt=""
@@ -59,7 +59,7 @@ const PropertyList = () => {
           <h2>33,389 cabins</h2>
         </div>
       </div>
-      <div className="pListItem col-md-2">
+      <div className="pListItem col-lg-4 col-md-4 col-sm-4 col-xs-2">
         <img
           src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/bg_cottages/5e1fd9cd716f4825c6c7eac5abe692c52cc64516.jpg"
           alt=""
@@ -72,7 +72,9 @@ const PropertyList = () => {
       </div>
       </div>
     </div>
+
   );
 };
 
 export default PropertyList;
+
